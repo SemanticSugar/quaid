368f4edad9f7fb11b8596f4a9c62b12fb84b8077
Ben Ogle <ogle.ben@gmail.com>
2011-02-19 14:34:10 -0800

    add backbone integration code i've been using

--- src/extension/backbone.js
+++ src/extension/backbone.js
@@ -32,8 +32,16 @@ if(!this.Backbone){
     return;
 }
 
+//give our module class backbone events!
+$.extend(Q.Module.prototype, Backbone.Events);
+$.extend(Q, Backbone.Events);
+
+
 var _View = Q.Module.extend(Backbone.View.prototype);
 Q.View = _View.extend('View', {
+    //in your base class, define:
+    //template: '#my-template',
+    
     _ctor: function(arg1, arg2){
         
         // If an element is specified (i.e. $('#blah').View()), place it in the 
@@ -50,11 +58,154 @@ Q.View = _View.extend('View', {
         // call the quaid constructor
         if ( $.isFunction(this.init) )
             this.init.call(this, this.el.jquery ? this.el : $(this.el), arg1);
+    },
+    
+    renderTemplate: function(attributes){
+        /**
+         * Uses the template property of the object, and passes in your attributes
+         */
+        this.container.html(_.template($(this.template).html(), attributes));
+        return this;
+    },
+    
+    render: function(){
+        return this.renderTemplate(this.model.attributes);
     }
 });
 
-//give our module class backbone events!
-$.extend(Q.Module.prototype, Backbone.Events);
-$.extend(Q, Backbone.Events);
+// using call as a hack so the resulting objects are of type Backbone.Collection
+// making it so isinstance still works.
+var _Collection = Class.extend.call(Backbone.Collection, Backbone.Collection.prototype);
+Q.Collection = _Collection.extend({
+    _ctor: function(models, settings){
+        
+        this.settings = settings;
+        
+        //call backbone's constructor
+        Backbone.Collection.call(this, models, settings);
+        
+        // call the quaid constructor
+        if ( $.isFunction(this.init) )
+            this.init.call(this, models, settings);
+    },
+    init: function(){},
+    
+    removeAll: function(){
+        var models = _.clone(this.models);
+        for(var i = 0; i < models.length; i++)
+            this.remove(models[i]);
+    },
+    
+    /**
+     * Called by Backbone.sync. grabs a url from this.urls based on the method
+     */
+    url: function(method) {
+        if(!this.urls || !this.urls[method])
+            $.error("Place a url map in the settings of your model! " + this + " needs a url for " + method);
+        else{
+            var url = this.urls[method];
+            return $.isFunction(url) ? url() : url;
+        }
+        throw new Error('Place a url map in the settings of your model!');
+    }
+});
+
+// using call as a hack so the resulting objects are of type Backbone.Model
+// making it so isinstance still works.
+var _Model = Class.extend.call(Backbone.Model, Backbone.Model.prototype);
+Q.Model = _Model.extend({
+    
+    _ctor: function(attributes, settings){
+        
+        this.settings = $.extend({}, Q.Model.defaults, settings);
+        
+        //call backbone's constructor
+        Backbone.Model.call(this, attributes, this.settings);
+        
+        // call the quaid constructor
+        if ( $.isFunction(this.init) )
+            this.init.call(this, attributes, this.settings);
+    },
+    
+    init: function(){},
+    
+    /**
+     * Upon create, update, read, this is called with the server's response.
+     * You are supposed to return an object that will be passed to this
+     * model's set function.
+     *
+     * Default it sets nothing returning an empty object.
+     */
+    parse: function(data){
+        data = data.results || data;
+        if(data.eid)
+            return {id: data.eid, eid: data.eid};
+        
+        return {};
+    },
+    
+    /**
+     * Called by Backbone.sync. Default behaviour is to reuturn all the attributes.
+     */
+    toJSON: function(){
+        return $.extend({}, this.attributes);
+    },
+    
+    /**
+     * Called by Backbone.sync. grabs a url from this.settings.urls based on the method
+     */
+    url: function(method) {
+        if(!this.urls || !this.urls[method])
+            $.error("Place a url map in the settings of your model! " + this + " needs a url for " + method);
+        else{
+            var url = this.urls[method];
+            return $.isFunction(url) ? url() : url;
+        }
+        throw new Error('Place a url map in the settings of your model!');
+    },
+    
+    /**
+     * returns GET/POST, etc based on a CRUD word from Backbone.sync
+     */
+    httpMethod: function(method){
+        return this.settings.methods[method];
+    }
+});
+
+Q.Model.methods = {
+    create: 'POST',
+    read: 'GET',
+    update: 'POST',
+    'delete': 'POST'
+};
+
+//end stuff to put in quaid
+
+//backbone sync override
+Backbone.sync = function(method, model, success, error) {
+    var modelJSON = model.toJSON(method);
+    
+    var wrap = function(fn, err){
+        return function(){
+            if($.isFunction(fn)) fn.apply(this, arguments);
+            model.trigger('request:end', err, arguments);
+        };
+    }
+    
+    // Default JSON-request options.
+    var params = {
+        url: model.url(method),
+        type: Q.Model.methods[method],
+        data: modelJSON,
+        dataType: 'json',
+        success: wrap(success, false),
+        error: wrap(error, true)
+    };
+    
+    model.trigger('request:start', modelJSON, params);
+    
+    // Make the request.
+    $.ajax(params);
+};
 
 })(jQuery);
